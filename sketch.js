176fd484c1e2e50e8be2e3086bf969338db4a4bc
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-14 17:37:04 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 var speech;
+var font = 'Helvetica';
+
 window.onload = function(){
 	speech = new p5.SpeechRec();
 	speech.onResult = function(){
-		//speech.resultString에 글꼴 주기
+		text=speech.resultString
+		textFont(font);
 		document.body.innerHTML = "You said: <h2>"+speech.resultString+"</h2>";
 		document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
 	}
