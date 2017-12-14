51c23ce2607060a7f61cffb41eb982ede4f765c4
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-14 23:00:54 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
-function setup(){
-	createCanvas(200,200);
-	fill(200);
+function setup() {
+  //createCanvas(windowWidth, windowHeight);
+  
+	noCanvas();
+  //function preload() {
+  	//myFont = loadFont('assets/AvenirNextLTPro-Demi.otf');}
+  var speech;
+  speech = new p5.SpeechRec();
+  speech.onResult = function() {
+    
+    //textFont(myFont);
+    document.body.innerHTML = "<h2> " + speech.resultString + "</h2>";
+    document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
+  }
+  speech.start();
 }
 
-var speech;
-
-window.onload = function(){
-	speech = new p5.SpeechRec();
-	speech.onResult = function(){
-		document.body.innerHTML = "You said: <h2>"+speech.resultString+"</h2>";
-		document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
-	}
-	speech.start();
-};
-function listen(){
-	speech.start();	
+function listen() {
+  speech.start();
 }
