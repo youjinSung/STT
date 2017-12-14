2a885e47ebc0dd49cc9748b92c82ad116e2a9433
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-14 17:32:35 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var speech;
 window.onload = function(){
 	speech = new p5.SpeechRec();
 	speech.onResult = function(){
+		//speech.resultString에 글꼴 주기
 		document.body.innerHTML = "You said: <h2>"+speech.resultString+"</h2>";
 		document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
 	}
