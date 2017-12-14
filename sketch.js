7ee2c930bdfb29ad5c7c78b580fd0c2f718504b5
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-14 17:43:06 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,8 @@
 var speech;
-var font = 'Helvetica';
 
 window.onload = function(){
 	speech = new p5.SpeechRec();
 	speech.onResult = function(){
-		text=speech.resultString
-		textFont(font);
 		document.body.innerHTML = "You said: <h2>"+speech.resultString+"</h2>";
 		document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
 	}
