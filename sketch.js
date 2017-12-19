3fd9551c47ae3a0a60c4eaf6fcbe4402888dcd68
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-19 17:47:22 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,6 @@ function setup() {
   speech = new p5.SpeechRec();
   speech.onResult = function() {
     
-
-    this.body.style.color='red';
     
     document.body.innerHTML ="<h2>"+speech.resultString+"</h2>";
     document.body.innerHTML += "<div><button onclick=\"listen()\">Click to listen again</button></div>";
