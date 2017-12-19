c525c71833b5e4291c87b8c1eb1e278267d92d17
youjinSung <32293602+youjinSung@users.noreply.github.com>
2017-12-19 17:38:36 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 function setup() {
-  //createCanvas(windowWidth, windowHeight);
+
   noCanvas();
-  //function preload() {
-  	//myFont = loadFont('assets/AvenirNextLTPro-Demi.otf');}
+
   var speech;
   speech = new p5.SpeechRec();
   speech.onResult = function() {
     
-    //textFont(myFont);
+
     this.body.style.color='red';
     
     document.body.innerHTML ="<h2>"+speech.resultString+"</h2>";
